42ead892e978c4f903bfb3badfa7aa8a55ad5e93
akashrev <akash.jain@reverieinc.com>
2016-11-18 17:03:34 +0530

    files modified

--- mysite/static/js/style.js
+++ mysite/static/js/style.js
@@ -1,10 +1,9 @@
 
 
 function show()
-{
-if(document.getElementById('expand').style.display == 'none')
-	document.getElementById('expand').style.display = 'block';
-else
-	document.getElementById('expand').style.display = 'none';
-}
-
+    {
+    if(document.getElementById('expand').style.display == 'none')
+        document.getElementById('expand').style.display = 'block';
+    else
+        document.getElementById('expand').style.display = 'none';
+    }
